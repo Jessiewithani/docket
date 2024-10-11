3d9fadcce174cb22ad4a516d847f08fba508d91c
jessie <jessie@jessies-MacBook-Pro.local>
2024-10-11 17:18:03 -0600

    Create form and card component with simple layout

---
+++ app/components/InputForm.tsx
@@ -0,0 +1,10 @@
+const InputForm = () => {
+  return (
+    <form className="flex gap-4">
+      <input />
+      <button className="bg-white text-black p-2">Submit</button>
+    </form>
+  )
+}
+
+export default InputForm;
---
+++ app/components/NameCard.tsx
@@ -0,0 +1,9 @@
+const NameCard = () => {
+  return (
+    <div className="border-2 border-white p-4">
+      <h1>Name: Jessie</h1>
+    </div>
+  );
+}
+
+export default NameCard;
