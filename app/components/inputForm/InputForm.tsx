6bae0c7f065e3d915c3cadb094ab2103e412f35f
jessie <jessiethanh.02@gmail.com>
2024-11-08 19:58:07 -0700

    Error handling

--- app/components/inputForm/InputForm.tsx
+++ app/components/inputForm/InputForm.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import * as styles from './form.tailwind';
 
 interface InputValues {
@@ -17,12 +17,59 @@ interface Props {
 
 const InputForm: React.FC<Props> = ({ inputValue, setInputValue, setSavedUserInfo }) => {
 
+  const [errors, setErrors] = useState({
+    phoneNumber: '',
+    birthdate: '',
+    email: ''
+  })
+
+  const validatePhoneNumber = (number: string) => {
+    console.log(`|${inputValue.phoneNumber}|`)
+    const phoneRegex = /^\(?([0-9]{3})\)?[-.\s]?([0-9]{3})[-.\s]?([0-9]{3})$/
+
+    //input.phoneNumber --M matches phoneRegex return true
+    if (number.match(phoneRegex)) {
+      console.log('number', inputValue.phoneNumber)
+      return true
+    }
+    return false
+  }
+
+  const validateEmail = (email: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email.match(emailRegex)) {
+      return true
+    }
+    return false
+  }
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setInputValue((prevValues) => ({
       ...prevValues,
       [name]: value
     }))
+
+    if (!validatePhoneNumber(inputValue.phoneNumber)) {
+      setErrors((prevError) => ({
+        ...prevError, phoneNumber: 'Phone number must be 7 digits'
+      }))
+    } else {
+      setErrors((prevError) => ({
+        ...prevError, phoneNumber: ''
+      }))
+    }
+
+    if (!validateEmail(inputValue.email)) {
+      setErrors((prevError) => ({
+        ...prevError, email: 'Email is invalid!'
+      }))
+    } else {
+      setErrors((prevError) => ({
+        ...prevError, email: ''
+      }))
+    }
   }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -46,7 +93,8 @@ const InputForm: React.FC<Props> = ({ inputValue, setInputValue, setSavedUserInf
       <div className={styles.inputFields_tw}>
         <div className={styles.labelInputContainer_tw}>
           <label className={styles.label_tw}>Phone number</label>
-          <input name="phoneNumber" onChange={handleChange} placeholder="ex: 720-251-6378" className="w-[300px] p-2 border rounded-md" value={inputValue?.phoneNumber}/>
+          <input name="phoneNumber" onChange={handleChange} placeholder="ex: 720-251-6378" className="w-[300px] p-2 border rounded-md" value={inputValue?.phoneNumber} />
+          {errors.phoneNumber && <p className="text-red-600">{errors.phoneNumber}</p>}
         </div>
         <div className={styles.labelInputContainer_tw}>
           <label className={styles.label_tw}>Birthdate</label>
@@ -55,7 +103,8 @@ const InputForm: React.FC<Props> = ({ inputValue, setInputValue, setSavedUserInf
       </div>
       <div className={styles.email_tw}>
         <label className={styles.label_tw}>Email</label>
-        <input name="email" onChange={handleChange} placeholder="ex: jessiethanh.02@gmail.com" className="p-2 border rounded-md w-full" value={inputValue?.email}/>
+        <input name="email" onChange={handleChange} placeholder="ex: jessiethanh.02@gmail.com" className="p-2 border rounded-md w-full" value={inputValue?.email} />
+        {errors.email && <p className="text-red-600">{errors.email}</p>}
       </div>
       <button className={styles.button_tw} type="submit">Submit</button>
     </form>
